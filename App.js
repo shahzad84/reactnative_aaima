3e191458c80e40e8ea127a06c427df891918c1be
hilarious3 <shahzad20022002@gmail.com>
2023-09-13 12:35:46 +0530

    header component

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from "./src/header"
 
+import * as React from 'react';
+import { PaperProvider } from 'react-native-paper';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Header />
+    </PaperProvider>
   );
 }
 
