b96045cb1db2ba5b511b334064fe9fb4f909a0a1
hilarious3 <shahzad20022002@gmail.com>
2023-09-13 17:28:13 +0530

    navigation initallization

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import Header from "./src/header";
 import Buttons from "./src/button";
 import * as React from "react";
 import { PaperProvider } from "react-native-paper";
+import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <PaperProvider>
-      {/* <Header /> */}
-      <Buttons />
-    </PaperProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Header" component={Header} /> */}
+        <Stack.Screen name="Button" component={Buttons} />
+      </Stack.Navigator>/
+    </NavigationContainer>
+    // <PaperProvider>
+    //   {/* <Header /> */}
+    //   <Buttons />
+    // </PaperProvider>
   );
 }
 
